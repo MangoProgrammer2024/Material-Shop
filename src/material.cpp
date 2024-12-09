b3f1d2296f5e5d19f75fb97d83b4b6c4765cb9ed
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-09 12:13:16 -0600

    Update material.cpp

--- src/material.cpp
+++ src/material.cpp
@@ -1,3 +1,15 @@
 /*
  MATERIAL.CPP
 */
+
+int game_descriptorMaterialId = 0;
+
+#ifdef MATERIAL_BUFFER
+const char * i_Mtl_Tag(Material * mMtl){
+   static char mtlBuffer[1024];
+    if(nMtl->MtlPtr.LoadMtl(mMtl) == true){
+
+    };
+};
+
+#endif
