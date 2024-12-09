9dadbc35107979d1576ddd298ffc4d5645df1f50
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-09 12:04:33 -0600

    Update material.h

--- includes/material.h
+++ includes/material.h
@@ -11,8 +11,11 @@ public:
 
     int MaterialId;
     bool MtlLoad(Material m);
+    Material MtlPtr;
     //material name
     const std::string& MaterialName;
+    void mtlPreference(float * _FLOAT);
+    void mtlTag(const std::string& _TAG);
     //material dimensions type
     float MtlW;
     float MtlH;
@@ -22,16 +25,33 @@ public:
     }MaterialType;
     //
     //Material Stuff
-    void * printmtlf(const std::string& Name, enum type);
+    void& printmtlf(const std::string& Name, enum type);
+    //MtlFile
     void MtlFile(Material * mtl, const std::string& extension){
        int i;
        for(i = 0; i >= 0; i++){
           extension == "*.gmtl";
-           mtl->MaterialName == ""extension;
+           mtl->MaterialName == "" extension;
        }
-       
+       MtlPtr.printmtlf(mtl->MaterialName,mtl->MTL_DESC);
        return& m;
     };
+
+    //MtlCode 
+    void MtlCode(Material * mtl);
+
+    //LoadMtl
+    void MtlLoad(Material mtl){
+       Material * mtlpref;
+       if(mtl.MtlLoad(mtl) == true){
+          mtl.mtlPreference(mtlpref->MtlW);//material width
+          mtl.mtlPreference(mtlpref->MtlH);//material height
+          mtl.mtlTag(mtlpref->MaterialName().begin);//material iterator
+       };
+       return& m;
+    };
+    //material info * binary data
+    void MtlInfo();
 
 
 
